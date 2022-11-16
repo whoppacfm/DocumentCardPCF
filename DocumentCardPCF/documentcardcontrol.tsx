58fde7f09af22302fd3535ec6a7161ba746c21fc
whoppacfm <13801775+whoppacfm@users.noreply.github.com>
2022-11-16 10:02:14 +0100

    Add files via upload

--- DocumentCardPCF/documentcardcontrol.tsx
+++ DocumentCardPCF/documentcardcontrol.tsx
@@ -20,6 +20,15 @@ import {
   import { ImageFit } from '@fluentui/react/lib/Image';
 import { TestImages } from '@fluentui/example-data';
 
+//FocusZone
+import { createArray } from '@fluentui/react/lib/Utilities';
+import { FocusZone, FocusZoneDirection } from '@fluentui/react-focus';
+import { DefaultButton } from '@fluentui/react/lib/Button';
+
+//Stack
+import { Stack } from '@fluentui/react/lib/Stack';
+
+//Custom css
 import './documentcardcontrol.css';
 
 //----------------------------
@@ -36,7 +45,8 @@ var CRM_TEST_MODE = 0;
 //Document Card
 //-------------
 const DocumentCardControl : React.FunctionComponent = (props:any) => {
-    const previewProps: IDocumentCardPreviewProps = {
+    
+      const previewProps: IDocumentCardPreviewProps = {
         getOverflowDocumentCountText: (overflowCount: number) => `+${overflowCount} more`,
         previewImages: [
           {
@@ -114,10 +124,41 @@ const DocumentCardControl : React.FunctionComponent = (props:any) => {
         ],
       };
       
+      const previewPropsTest: IDocumentCardPreviewProps = {
+        getOverflowDocumentCountText: (overflowCount: number) => `+${overflowCount} more`,
+        previewImages: [
+          {
+            name: 'IDocumentCardPreviewProps',
+            linkProps: {
+              href: 'http://bing.com',
+              target: '_blank',
+            },
+            previewImageSrc: TestImages.documentPreview,
+            iconSrc: TestImages.iconPpt,
+            imageFit: ImageFit.cover,
+            width: 318,
+            height: 196,
+          },
+          {
+            name: 'IDocumentCardPreviewProps 2',
+            linkProps: {
+              href: 'http://bing.com',
+              target: '_blank',
+            },
+            previewImageSrc: TestImages.documentPreview,
+            iconSrc: TestImages.iconPpt,
+            imageFit: ImageFit.cover,
+            width: 318,
+            height: 196,
+          },          
+        ],
+      };          
+
       const onActionClick = (action: string, ev: React.SyntheticEvent<HTMLElement>): void => {
         console.log(`You clicked the ${action} action`);
         ev.stopPropagation();
         ev.preventDefault();
+        alert(`You clicked the ${action} action`);
       };
       
       const documentCardActions = [
@@ -138,29 +179,131 @@ const DocumentCardControl : React.FunctionComponent = (props:any) => {
         },
       ];
 
+      const documentCardActionsTest = [
+        {
+          iconProps: { iconName: 'AccessLogo' },
+          onClick: onActionClick.bind(this, 'share'),
+          ariaLabel: 'share action',
+        },
+        {
+          iconProps: { iconName: 'AccountActivity' },
+          onClick: onActionClick.bind(this, 'pin'),
+          ariaLabel: 'pin action',
+        },
+        {
+          iconProps: { iconName: 'AADLogo' },
+          onClick: onActionClick.bind(this, 'notifications'),
+          ariaLabel: 'notifications action',
+        },
+        {
+          iconProps: { iconName: 'AutoRacing' },
+          onClick: onActionClick.bind(this, 'notifications'),
+          ariaLabel: 'notifications action',
+        },
+        {
+          iconProps: { iconName: 'ArrangeSendToBack' },
+          onClick: onActionClick.bind(this, 'notifications'),
+          ariaLabel: 'notifications action',
+        },                
+      ];      
+
+      const tokens = { childrenGap: 2 };
+
     return (
         <>
-        <DocumentCard
-            aria-label="Document Card with multiple items, commands and views. Marketing documents. 6 files were uploaded.
-                            Created by Annie Lindqvist in February 23, 2016. 432 views."
-        >
-            <DocumentCardPreview {...previewProps} />
-            <DocumentCardLocation
-                location="Marketing Documents"
-                locationHref="http://microsoft.com"
-                ariaLabel="Location, Marketing Documents"
-            />
-            <DocumentCardTitle title="6 files were uploaded" />
-            <DocumentCardActivity
-                activity="Created Feb 23, 2016"
-                people={[{ name: 'Annie Lindqvist', profileImageSrc: TestImages.personaFemale }]}
-            />
-            <DocumentCardActions actions={documentCardActions} views={432} />
-        </DocumentCard>        
+          {/*<FocusZone direction={FocusZoneDirection.domOrder} role="menubar">*/}
+          <Stack tokens={tokens} horizontal verticalAlign="center" style={{overflow:"auto", scrollbarWidth:"thin", width:"1200px", border:"1px solid black"}}>
+            <DocumentCard key="card1" role="menuitem"
+                aria-label="Document Card with multiple items, commands and views. Marketing documents. 6 files were uploaded.
+                                Created by Annie Lindqvist in February 23, 2016. 432 views."
+            >
+                <DocumentCardPreview {...previewProps} />
+                <DocumentCardLocation
+                    location="Marketing Documents"
+                    locationHref="http://microsoft.com"
+                    ariaLabel="Location, Marketing Documents"
+                />
+                <DocumentCardTitle title="6 files were uploaded" />
+                <DocumentCardActivity
+                    activity="Created Feb 23, 2016"
+                    people={[{ name: 'Annie Lindqvist', profileImageSrc: TestImages.personaFemale }]}
+                />
+                <DocumentCardActions actions={documentCardActions} views={432} />
+            </DocumentCard>
+            <DocumentCard aria-label="aria-label" key="card2" role="menuitem">
+                <DocumentCardPreview {...previewPropsTest} />
+                <DocumentCardLocation
+                    location="DocumentCardLocation"
+                    locationHref="http://microsoft.com"
+                    ariaLabel="DocumentCardLocation Label"
+                />
+                <DocumentCardTitle title="DocumentCardTitle" />
+                <DocumentCardActivity
+                    activity="DocumentCardActivity"
+                    people={[{ name: 'DocumentCardActivity People', profileImageSrc: TestImages.personaMale }]}
+                />
+                <DocumentCardActions actions={documentCardActionsTest} views={233} />
+            </DocumentCard>
+            <DocumentCard aria-label="aria-label" key="card2" role="menuitem">
+                <DocumentCardPreview {...previewPropsTest} />
+                <DocumentCardLocation
+                    location="DocumentCardLocation"
+                    locationHref="http://microsoft.com"
+                    ariaLabel="DocumentCardLocation Label"
+                />
+                <DocumentCardTitle title="DocumentCardTitle" />
+                <DocumentCardActivity
+                    activity="DocumentCardActivity"
+                    people={[{ name: 'DocumentCardActivity People', profileImageSrc: TestImages.personaMale }]}
+                />
+                <DocumentCardActions actions={documentCardActionsTest} views={233} />
+            </DocumentCard>
+            <DocumentCard aria-label="aria-label" key="card2" role="menuitem">
+                <DocumentCardPreview {...previewPropsTest} />
+                <DocumentCardLocation
+                    location="DocumentCardLocation"
+                    locationHref="http://microsoft.com"
+                    ariaLabel="DocumentCardLocation Label"
+                />
+                <DocumentCardTitle title="DocumentCardTitle" />
+                <DocumentCardActivity
+                    activity="DocumentCardActivity"
+                    people={[{ name: 'DocumentCardActivity People', profileImageSrc: TestImages.personaMale }]}
+                />
+                <DocumentCardActions actions={documentCardActionsTest} views={233} />
+            </DocumentCard>
+            <DocumentCard aria-label="aria-label" key="card2" role="menuitem">
+                <DocumentCardPreview {...previewPropsTest} />
+                <DocumentCardLocation
+                    location="DocumentCardLocation"
+                    locationHref="http://microsoft.com"
+                    ariaLabel="DocumentCardLocation Label"
+                />
+                <DocumentCardTitle title="DocumentCardTitle" />
+                <DocumentCardActivity
+                    activity="DocumentCardActivity"
+                    people={[{ name: 'DocumentCardActivity People', profileImageSrc: TestImages.personaMale }]}
+                />
+                <DocumentCardActions actions={documentCardActionsTest} views={233} />
+            </DocumentCard>
+            <DocumentCard aria-label="aria-label" key="card2" role="menuitem">
+                <DocumentCardPreview {...previewPropsTest} />
+                <DocumentCardLocation
+                    location="DocumentCardLocation"
+                    locationHref="http://microsoft.com"
+                    ariaLabel="DocumentCardLocation Label"
+                />
+                <DocumentCardTitle title="DocumentCardTitle" />
+                <DocumentCardActivity
+                    activity="DocumentCardActivity"
+                    people={[{ name: 'DocumentCardActivity People', profileImageSrc: TestImages.personaMale }]}
+                />
+                <DocumentCardActions actions={documentCardActionsTest} views={233} />
+            </DocumentCard>                                                
+          </Stack>
         </>
     );
 }
-
 
 export function Render(context:any, container:any, theobj:object) {
   
